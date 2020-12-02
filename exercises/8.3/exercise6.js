3873266837349250151197a9a9586b5a75a7c736
Joao <joaocarlitoneto@gmail.com>
2020-12-02 19:19:41 -0300

    Day 8.3 - EX06

--- exercises/8.3/exercise6.js
+++ exercises/8.3/exercise6.js
@@ -5,7 +5,10 @@ const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 
 function studentAverage() {
-  // escreva seu código aqui
+  return students.map((student, index) => ({
+    name: student,
+    average: grades[index].reduce((sum, cur) => sum + cur) / grades[index].length
+  }));
 }
 
 /*Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas } . Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos , aqui além de reduce será necessário utilizar também a função map . Dica: Você pode acessar o index do array dentro de map , e você pode ver o objeto esperado na constante expected .*/
