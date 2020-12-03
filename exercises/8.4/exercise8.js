a75d891bb955d22da4afb225bcd47b5f0bdc40a5
Joao <joaocarlitoneto@gmail.com>
2020-12-03 19:15:11 -0300

    Enviando exercícios dia 8.4

---
+++ exercises/8.4/exercise8.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+
+// escreva greet abaixo
+const greet = (name, message = 'Hi') => `${message} ${name}`;
+
+assert.strictEqual(greet("John"), "Hi John")
+assert.strictEqual(greet("John", "Good morning"), "Good morning John")
+assert.strictEqual(greet("Isabela", "Oi"), "Oi Isabela")
+
+/*
+Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+Dica: use default params .
+*/
