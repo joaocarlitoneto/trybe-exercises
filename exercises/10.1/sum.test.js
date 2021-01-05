6c6d94d8ff0890426e847eb8b21e8232b82577f2
Joao <joaocarlitoneto@gmail.com>
2021-01-05 15:28:23 -0300

    parte 1

--- exercises/10.1/sum.test.js
+++ exercises/10.1/sum.test.js
@@ -1,10 +1,6 @@
 const sum = require('./sum');
 
 describe('Exercício 1 ao 5 SUM', () => {
-  test('A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
-    expect(sum(1,3)).toBe(4);
-  });
-
   test('Teste se o retorno de sum(4, 5) é 9', () => {
     expect(sum(4,5)).toBe(9);
   });
@@ -13,7 +9,11 @@ describe('Exercício 1 ao 5 SUM', () => {
     expect(sum(0,0)).toBe(0);
   });
 
-  test('Teste se a função sum lança um erro quando os parametros são 4 e "5" (string 5)', () => {
+  test('Teste se a função sum lança um erro quando os parâmetros são 4 e "5" (string 5)', () => {
     expect(() => sum(4,'5')).toThrow();
+  });
+
+  test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
+    expect(() => sum(4,'5')).toThrow('parameters must be numbers');
   });
 })
