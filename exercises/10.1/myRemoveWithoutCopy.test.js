7407083b8561658e2a2d450f356daa3bbb620b1f
Joao <joaocarlitoneto@gmail.com>
2021-01-05 20:32:55 -0300

    Encode e Decode

--- exercises/10.1/myRemoveWithoutCopy.test.js
+++ exercises/10.1/myRemoveWithoutCopy.test.js
@@ -1,9 +1,9 @@
 const myRemoveWithoutCopy = require('./myRemoveWithoutCopy');
 
 describe('myRemoveWithoutCopy', () => {
-  // test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
-  //   expect(myRemoveWithoutCopy([1,2,3,4],3)).toEqual([1,2,4]);
-  // });
+  test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1,2,3,4],3)).toEqual([1,2,4]);
+  });
 
   test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 3) não retorna o array [1, 2, 3, 4]', () => {
     expect(myRemoveWithoutCopy([1,2,3,4],3)).not.toEqual([1,2,3,4]);
@@ -15,7 +15,7 @@ describe('myRemoveWithoutCopy', () => {
     expect(arr).toEqual([2,3,4]);
   });
 
-  // test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
-  //   expect(myRemoveWithoutCopy([1,2,3,4],5)).toEqual([1,2,3,4]);
-  // });
+  test('Verifique se a chamada myRemoveWithoutCopy([1, 2, 3, 4], 5) retorna o array esperado', () => {
+    expect(myRemoveWithoutCopy([1,2,3,4],5)).toEqual([1,2,3,4]);
+  });
 })
